f385ec3248c109e2945b7d83338995418a69f84e
Aleksey <misian20@gmail.com>
2023-09-15 22:18:37 +0300

    сохранил варинат решения задачи который предлагают на сайте

--- task2/Program.cs
+++ task2/Program.cs
@@ -174,3 +174,33 @@
 // int x = WorkWithUser("Введите точку X : ");
 // int y = WorkWithUser("Введите точку Y : ");
 // Console.WriteLine(FundQuarter(x,y));
+
+// using System;
+
+// public class Answer
+// {
+//     static void ShowCube(int N)
+//     {
+//         for (int i = 1; i <= N; i++)
+//         {
+//             Console.WriteLine(Cube(i));
+//         }
+//     }
+
+//     public static int Cube(int number)
+//     {
+//         return number * number * number;
+//     }
+
+//       static public void Main(string[] args) {
+//         int N;
+
+//         if (args.Length >= 1) {
+//             N = int.Parse(args[0]);
+//         } else {
+//             N = 6;
+//         }
+
+//         ShowCube(N);
+//     }
+// }
